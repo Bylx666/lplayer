a5c478785b534e68d749a5fe7cbe7f693db9a485
Subkey <2822448396@qq.com>
2021-12-26 00:56:42 +0800

    add: dev suit

--- lplayer.js
+++ lplayer.js
@@ -2,15 +2,33 @@
 function LPlayer() { 
 if(document.getElementsByTagName('lplayer').length = 1) 
     var p = document.getElementsByTagName('lplayer')[0]
-p.innerHTML = ''
-var xhr = new XMLHttpRequest()
-    xhr.open('get','/index.html')
-    xhr.send()
-    xhr.onreadystatechange = function (){
-        if(xhr.status == 200 && xhr.readyState == 4)
-        console.log(xhr.responseText)
+var songData
+
+// stop 'online' function while local UI developing
+// For security, we prevent javascript while using iframe to test the UI in index.html.
+// Please upload your file online and use XMLHttpRequest method to test scripts for new UI.
+
+online()
+
+function online(){
+    function get(url) { 
+        var xhr = new XMLHttpRequest()
+        xhr.open('get',url)
+        xhr.send()
+        xhr.onreadystatechange = function (){
+            if(xhr.status == 200 && xhr.readyState == 4)
+            return xhr.responseText
+        }
     }
 
+    p.innerHTML = get('https://cdn.jsdelivr.net/gh/Bylx666/lplayer@main/index.html')
+    songData = JSON.parse(get('https://cdn.jsdelivr.net/gh/Bylx666/lplayer@main/demo/songs.json'))
 }
+
+function d() {  }
+
+
+}
+
 
 LPlayer()
