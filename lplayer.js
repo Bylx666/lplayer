3d32b6c13db59442375e9509437515f51bbfbec5
Subkey <2822448396@qq.com>
2021-12-25 23:31:46 +0800

    dev test files update

--- lplayer.js
+++ lplayer.js
@@ -1,7 +1,16 @@
-LPlayer()
+
 function LPlayer() { 
 if(document.getElementsByTagName('lplayer').length = 1) 
     var p = document.getElementsByTagName('lplayer')[0]
 p.innerHTML = ''
-p
+var xhr = new XMLHttpRequest()
+    xhr.open('get','/index.html')
+    xhr.send()
+    xhr.onreadystatechange = function (){
+        if(xhr.status == 200 && xhr.readyState == 4)
+        console.log(xhr.responseText)
+    }
+
 }
+
+LPlayer()
